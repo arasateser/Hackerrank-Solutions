b3316f85a0726bbda09ce2ad4facb594615bbd8d
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2024-03-15 18:18:57 +0300

    Update ollivandersInventory.sql

--- SQL/ollivandersInventory.sql
+++ SQL/ollivandersInventory.sql
@@ -7,5 +7,10 @@ WHERE wp.is_evil = 0 and w.coins_needed = (
     WHERE w1.power = w.power and wp1.age = wp.age) 
 ORDER BY w.power desc, wp.age desc
 
+/*FIRST WHERE CLAUSE:
+The condition wp.is_evil = 0 filters the rows where the wand is not marked as evil.
+The condition w.coins_needed = (SELECT MIN(coins_needed) ...) filters the rows where the coins needed 
+to purchase the wand (w.coins_needed) are equal to the minimum coins needed among wands with the same power and age.
 
-
+The subquery (SELECT MIN(coins_needed) ...) calculates the minimum coins needed among wands with the same power and 
+age. It correlates the subquery with the outer query by matching w1.power = w.power and wp1.age = wp.age.*/
